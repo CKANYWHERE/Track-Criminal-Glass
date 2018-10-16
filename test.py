8d91d89735dc8e4d06291e2624307176d1006cf9
Chang-__- <janu723@naver.com>
2018-10-16 16:25:43 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+import cv2
+import face_recognition_models
+capture = cv2.VideoCapture(0)
+
+capture.set(3,320)
+capture.set(4,240)
+while(1):
+    print('opencv 동작중')
+    ret,frame = capture.read()
+    cv2.imshow('webcam',frame)
+    if cv2.waitKey(1) == ord('q'):
+        break;혓
+capture.release()
+cv2.destroyAllWindows()
